1a694c9b6642dc1d65709935dd56ed4d00256b84
LilMike707 <106935461+LilMike707@users.noreply.github.com>
2023-08-21 16:46:32 -0700

    asdasd

---
+++ pokemon/src/Pokecard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const POKE_API = 'https://raw.githubusercontent.com/' +
+'PokeAPI/sprites/master/sprites/pokemon/';
+
+function Pokecard(props){
+    let imgSrc = `${POKE_API}${props.id}.png`
+
+    return (
+        <div>
+            <div>{props.name}</div>
+            <img src={imgSrc}/>
+            <div>Type: {props.type}</div>
+            <div>EXP: props.exp</div>
+        </div>
+    )
+}
